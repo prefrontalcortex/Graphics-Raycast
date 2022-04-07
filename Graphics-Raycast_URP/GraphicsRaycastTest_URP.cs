7a26f48ebcaf8f2d3790b6a5786d3935f0b5b09f
Vincent Kreuziger <kreuziger@prefrontalcortex.de>
2022-04-07 17:57:27 +0200

    Setup Sample Scene with working implementation under URP
    Original setup doesn't work anymore since URP breaks shaders and some C# code

---
+++ Graphics-Raycast_URP/GraphicsRaycastTest_URP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GraphicsRaycastTest_URP : MonoBehaviour
+{
+    public float maxDistance = 100f;
+    public bool hitSomething;
+
+    private RaycastHit hit;
+
+    void Update()
+    {
+        hitSomething = GraphicsRaycast_URP.Raycast(this.transform.position, this.transform.forward, out hit, maxDistance);
+    }
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        GraphicsRaycast_URP.DrawGizmo(hitSomething, this.transform.position, this.transform.forward, hit, maxDistance, 1f);
+#endif
+    }
+}
